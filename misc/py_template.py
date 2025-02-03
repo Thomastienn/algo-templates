33f2352000b249a5de76247755a163608d293173
thomas <thomas.vu1@ucalgary.ca>
2025-02-03 00:11:22 -0700

    added py template

---
+++ misc/py_template.py
@@ -0,0 +1,13 @@
+import sys
+import math
+import heapq
+import bisect
+import re
+from sys import stdin,stdout
+from math import gcd,floor,sqrt,log
+from collections import defaultdict, Counter, deque, OrderedDict
+from bisect import bisect_left, bisect_right
+from itertools import permutations, combinations
+
+sys.setrecursionlimit(10**6)
+mod=1000000007
